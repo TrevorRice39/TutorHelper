871e1dd6eae46abfa2ec7d8e3fbdf1e7d2a9e659
TrevorRice39 <trevor_rice39@mymail.eku.edu>
2020-03-24 18:49:40 -0400

    added Student class and added the !addme command to add students to db

---
+++ Student.py
@@ -0,0 +1,18 @@
+
+class Student:
+
+
+    def __init__(self, discordID, course, assignmentDescription):
+        self.discordID = discordID
+        self.course = course
+        self.assignmentDescription = assignmentDescription
+    
+    def getDiscordID(self):
+        return self.discordID
+    def getCourse(self):
+        return self.course
+    def getAssignmentDescription(self):
+        return self.assignmentDescription
+
+    def __str__(self):
+        return self.getDiscordID
--- bot.py
+++ bot.py
@@ -8,6 +8,7 @@ from Tutor import Tutor
 from datetime import datetime
 import asyncio
 import access
+from Student import Student
 # dictionary of id's to member objects
 memberDict = dict()
 loop = asyncio.new_event_loop()
@@ -19,13 +20,17 @@ from discord.ext import commands
 client = discord.Client()
 load_dotenv()
 
+db_host = os.getenv('DB_HOST')
+db_user = os.getenv('DB_USER')
+db_pw = os.getenv('DB_PW')
+db_name = os.getenv('DB_NAME')
+con = access.Connection(db_host, db_user, '', db_name, False)
 client = discord.Client()
 # this code starts a separate threads that updates the current tutors every 15 minuntes
 currentTutors = []
 studentQueue = [] # students who are queued up to be tutored
 # maps the tutorDiscordID to the student and start time of the session
 currentTutoringDict = dict()
-print(Tutor.getAllTutors())
 def updateTutors():
     global currentTutors
     tutors = Tutor.getCurrentTutors()
@@ -56,22 +61,21 @@ def studentsAvailable():
     return len(studentQueue) > 0
 # every thirty seconds this function will run and check if a tutor is available and if there is a student that needs help
 async def updateQueues():
-    print(currentTutors)
-    print(studentQueue)
+    print('students ', studentQueue)
     print(currentTutoringDict)
 
     if len(studentQueue) > 0 and len(currentTutors) > 0:
         tutor = currentTutors.pop(0)
-        student = studentQueue.pop(0)
-
+        studentObject = studentQueue.pop(0)
+        studentID = studentObject.getDiscordID()
         
-        bot.loop.create_task(sendDM(tutor, f'{memberDict[student].mention} is waiting on your assistance!'))
-        bot.loop.create_task(sendDM(student, f'Hello {memberDict[student].mention}, {memberDict[tutor].mention} will contact you to help you!'))
+        bot.loop.create_task(sendDM(tutor, f'{memberDict[studentID].mention} is waiting on your assistance!\nCourse: {studentObject.getCourse()}\nDescription: {studentObject.getAssignmentDescription()}'))
+        bot.loop.create_task(sendDM(studentID, f'Hello {memberDict[studentID].mention}, {memberDict[tutor].mention} will contact you to help you!'))
         
         now = datetime.now()
         currentTime = f'{now.hour}:{now.minute}:{now.second}'
 
-        currentTutoringDict[tutor] = (student, currentTime)
+        currentTutoringDict[tutor] = (studentID, currentTime, studentObject.getCourse())
 
     await asyncio.sleep(1)
     bot.loop.create_task(updateQueues())
@@ -80,11 +84,6 @@ async def updateQueues():
         if guild.name == GUILD:
             break
 
-    # print(
-    #     f'{client.user} is connected to the following guild:\n'
-    #     f'{guild.name}(id: {guild.id})\n'
-    # )
-
     # build member dictionary
     for member in guild.members:
         if member not in memberDict:
@@ -92,7 +91,7 @@ async def updateQueues():
 
 @bot.command(name='info', help='Gives the user some info about how to use the bot')
 async def help(ctx):
-    help_text = "If you are a student and need help, type !tutorme to be scheduled for tutoring."
+    help_text = "If you are a student and need help, do the following:\nThe command you need to use is '!tutorme csc 123 here is a description of my assignment'. The description is optional but you must put your course prefix and number"
     response = help_text
     await ctx.send(response)
 
@@ -111,7 +110,7 @@ async def on_ready():
     for member in guild.members:
         if member not in memberDict:
             memberDict[str(member)] = member
-    print(memberDict)
+    # print(memberDict)
     # members = '\n - '.join([member.name for member in guild.members])
     # print(f'Guild Members:\n - {members}')
 @bot.event
@@ -121,17 +120,51 @@ async def on_member_join(member):
         f'Hi {member.name}, welcome to the EKU CSC Tutoring server! Look in the info channel for more information! If you want help using the tutoring bot type !info'
     )
 
-@bot.command(name='tutorme', help='Schedules a student for tutoring')
-async def tutor_me(ctx):
-    discordID = f'{ctx.message.author.name}#{ctx.message.author.discriminator}'
-    if discordID not in studentQueue:
-        studentQueue.append(discordID)
+def studentInQueue(studentID):
+    for student in studentQueue:
+        print(student.getDiscordID, " ", studentID)
+        if student.getDiscordID() == studentID:
+            return True
+    for value in list(currentTutoringDict.values()):
+        if value[0] == studentID:
+            return True
+    return False
 
+
+@bot.command(name='tutorme', help='Schedules a student for tutoring')
+async def tutor_me(ctx, *, arguments):
+    if len(arguments) <= 6:
+        await ctx.message.author.create_dm()
+        await ctx.message.author.dm_channel.send(
+            f'Hi {ctx.message.author.mention}, to use this command you must enter your course and optionally a description of your assignment. \nExample: !tutorme csc 195 I need help with homework 5.'
+        )
+        return
+    classArg = arguments[0 : 7].upper()
+    description = arguments[8:]
+    discordID = f'{ctx.message.author.name}#{ctx.message.author.discriminator}'
+    if studentInQueue(discordID) == False:
+        print('not in')
+        s = Student(discordID, classArg, description)
+        studentQueue.append(s)
+    else:
+        await ctx.message.author.create_dm()
+        await ctx.message.author.dm_channel.send(
+            f'Hi {ctx.message.author.mention}, you are already in queue or you are being tutored currently.'
+        )
+        return
     await ctx.message.author.create_dm()
     await ctx.message.author.dm_channel.send(
         f'Hi {ctx.message.author.mention}, please wait until a tutor becomes available and they will message you.'
     )
 
+@bot.command(name='addme', help="Adds a student to the db")
+async def add_me(ctx, fName, lName):
+    insertVals = [ctx.message.author, fName, lName]
+    con.insert("Student", 'discordID, firstName, lastName', insertVals)
+    await ctx.message.author.create_dm()
+    await ctx.message.author.dm_channel.send(
+        f'Hello {ctx.message.author.mention}, you have been added!'
+    )
 @bot.command(name='freetutor', help='Makes the tutor available and ends the session')
 async def free_tutor(ctx):
     # make sure it is a tutor using the command
@@ -141,18 +174,10 @@ async def free_tutor(ctx):
         now = datetime.now()
         currentTime = f'{now.hour}:{now.minute}:{now.second}'
 
-        load_dotenv()
-        db_host = os.getenv('DB_HOST')
-        db_user = os.getenv('DB_USER')
-        db_pw = os.getenv('DB_PW')
-        db_name = os.getenv('DB_NAME')
-        con = access.Connection(db_host, db_user, '', db_name, False)
-
         student = currentTutoringDict[discordID][0]
 
         ret = []
-        insertVals = [discordID, currentTutoringDict[discordID][1], currentTime, student, 'CSC 195']
-        print(insertVals)
+        insertVals = [discordID, currentTutoringDict[discordID][1], currentTime, student, currentTutoringDict[discordID][2]]
         con.insert("TutoringLogs", 'discordID, startTime, endTime, studentDiscordID, classTutored', insertVals)
         
         bot.loop.create_task(sendDM(student, f'Hello {memberDict[student].mention}, your tutoring session has ended.'))
